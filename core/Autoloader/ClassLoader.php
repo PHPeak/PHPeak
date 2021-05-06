9c5267fea50eae6c635cee40a5dcf28d4f3f7a1a
Esli Stavenga <EsliStavenga@users.noreply.github.com>
2021-05-06 23:07:09 +0200

    WIP: Moved autoloading stuff around

--- core/Autoloader/ClassLoader.php
+++ core/Autoloader/ClassLoader.php
@@ -7,15 +7,17 @@ use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use RecursiveRegexIterator;
 use ReflectionClass;
+use ReflectionException;
 use RegexIterator;
 
-class ClassLoader
+abstract class ClassLoader
 {
 
 	private static array $instances = [];
 
 	public static function loadServices(): void
 	{
+		//FIXME this is ugly
 		$baseDir = __DIR__ . '/../';
 
 		$dir = new RecursiveDirectoryIterator($baseDir);
@@ -23,19 +25,29 @@ class ClassLoader
 		$regexIterator = new RegexIterator($iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
 
 		foreach($regexIterator as $value) {
-			$className = 'PHPeak\\' .  str_replace([$baseDir, '.php', '/'], ['', '', '\\'], $value[0]);
+			$className = 'PHPeak\\' .  str_replace([$baseDir, '.php', DIRECTORY_SEPARATOR], ['', '', '\\'], $value[0]);
+			try {
+				$reflection = new ReflectionClass($className);
+			} catch (ReflectionException) {
+				continue;
+			}
 
-			$reflection = new ReflectionClass($className);
 			$isServiceClass = count(array_filter($reflection->getAttributes(), fn($x) => $x->getName() === ServiceAttribute::class)) === 1;
-
 			if($isServiceClass) {
 				self::$instances[] = new $className();
 			}
 		}
+	}
 
-		var_dump(self::$instances);
-		echo "<br />";
+	public static function registerAutoloader()
+	{
+		spl_autoload_register(function ($class) {
+			$file = sprintf('../core%s', str_replace([__NAMESPACE__, '\\'], ['', DIRECTORY_SEPARATOR], $class).'.php');
 
+			if (file_exists($file)) {
+				require_once($file);
+			}
+		});
 	}
 
 }
--- core/Kernel.php
+++ core/Kernel.php
@@ -2,43 +2,26 @@
 
 namespace PHPeak;
 
+//FIXME ewwwww
+require_once('Autoloader/ClassLoader.php');
+
 use PHPeak\Autoloader\ClassLoader;
-use PHPeak\HTTP\Request;
 
 class Kernel
 {
 
 	public function __construct()
 	{
-		$this->registerAutoloader();
-//TODO		$this->loadConfig();
+		ClassLoader::registerAutoloader();
+		ClassLoader::loadServices();
+
+		//cache
 	}
 
 	public function handleRequest(): string
 	{
-		$request = new Request();
 
-		ClassLoader::loadServices();
-
-
-		var_dump($request);
-		var_dump($_GET);
 
 		return '';
 	}
-
-	private function registerAutoloader()
-	{
-		//TODO fix this
-		spl_autoload_register(function ($class) {
-			$file = sprintf('../core%s', str_replace([__NAMESPACE__, '\\'], ['', DIRECTORY_SEPARATOR], $class).'.php');
-
-			if (file_exists($file)) {
-				require_once($file);
-				return true;
-			}
-			return false;
-		});
-	}
-
 }
